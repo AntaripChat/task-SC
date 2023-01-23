1349a7bfe98a56b6e029acc5fb273cf2ce28edd7
Antarip Chatterjee <chatterjeeantarip1234@gmail.com>
2023-01-23 17:01:19 +0530

    routes

--- routes/home.routes.js
+++ routes/home.routes.js
@@ -2,7 +2,7 @@ const {home,notFound} = require('../controller/index.controller')
 
 module.exports = function(app){
     app.get('/',home);
-    app.get('*',notFound)
+    //app.get('*',notFound)
     // app.post('/help',data);
     // app.get('/show',showdata);
 }
